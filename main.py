4659fb5480f3f5b60ffd319a0b74786351aa2779
Franco Ballesteros <ballestafranco@gmail.com>
2024-06-04 19:38:40 -0300

    2024

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def suma(numero1:int, numero2:int): 
     return numero1+numero2
-
-print("La suma es: ", suma(3,3))
+num1= 3
+num2= 3
+print("La suma entre ",num1," y ",num2," es: ", suma(num1,num2))
